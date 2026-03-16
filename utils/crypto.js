0dfe3b58dfc27daa7ea94da83ef3ec2a2602d3db
yare <ejimtpck@gmail.com>
2026-03-16 23:20:22 +0800

    增加/untils/crypto.js加密功能

---
+++ utils/crypto.js
@@ -0,0 +1,18 @@
+/**
+ * 將原始密碼轉換為安全的 SHA-256 雜湊字串
+ * @param {string} password - 使用者輸入的明文密碼
+ * @returns {Promise<string>} - 64 位元的十六進位雜湊字串
+ */
+export async function hashPassword(password) {
+  // 1. 使用TextEncoder()的encode，將字串轉換為位元組
+  const msgUint8 = new TextEncoder().encode(password);
+  
+  // 2. 進行加密運算，使用SHA-256演算法
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  
+  // 3. 轉換型別並輸出唯一連串16進位的字串
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    
+  return hashHex;
+}
